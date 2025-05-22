389dddd5c605b8a37541e117c0b665ac66e16ae6
Markus Haslinger <markus@haslinger.it>
2025-05-22 16:03:17 +0200

    add configuration of backend host

--- Angular_WebAPI/src/app/app.config.ts
+++ Angular_WebAPI/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import {
-  ApplicationConfig,
+  ApplicationConfig, EnvironmentProviders,
   provideExperimentalZonelessChangeDetection
 } from "@angular/core";
 import { provideRouter } from '@angular/router';
@@ -10,39 +10,53 @@ import {
   IncludeBearerTokenCondition, includeBearerTokenInterceptor,
   provideKeycloak
 } from "keycloak-angular";
-import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from "@angular/common/http";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ConfigService } from "../core/config.service";
 
-const authTokenCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
-  urlPattern: /^(http:\/\/localhost:5050)(\/.*)?$/i
-});
-const keycloakProvider = provideKeycloak({
-  config: {
-    url: 'https://auth.htl-leonding.ac.at', // URL of the Keycloak server
-    realm: 'htlleonding', // Realm to be used in Keycloak
-    clientId: 'htlleonding-service' // Client ID for the application in Keycloak,
-  },
-  initOptions: {
-    onLoad: 'check-sso', // Action to take on load
-    //enableLogging: true, // Enables logging
-    // IMPORTANT: implicit flow is no longer recommended, but using standard flow leads to a 401 at the keycloak server
-    // when retrieving the token with the access code - we leave it like this for the moment until a solution is found
-    flow: 'implicit'
-  },
-  providers: [
-    {
-      provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
-      useValue: [authTokenCondition] // Specify conditions for adding the Bearer token
-    }
-  ]
-});
+function provideConfiguredKeycloak(): EnvironmentProviders {
+  function createAuthTokenCondition(configService: ConfigService) {
+    const { protocol, hostname, port } = new URL(configService.config.backendBaseUrl);
+    const hostPort = port ? `${hostname}:${port}` : hostname;
+    const urlPattern = new RegExp(`^${protocol}//${hostPort}(\\/.*)?$`, 'i');
+    return createInterceptorCondition<IncludeBearerTokenCondition>({
+      urlPattern: urlPattern
+    });
+  }
+
+  return provideKeycloak({
+    config: {
+      url: 'https://auth.htl-leonding.ac.at', // URL of the Keycloak server
+      realm: 'htlleonding', // Realm to be used in Keycloak
+      clientId: 'htlleonding-service' // Client ID for the application in Keycloak,
+    },
+    initOptions: {
+      onLoad: 'check-sso', // Action to take on load
+      //enableLogging: true, // Enables logging
+      // IMPORTANT: implicit flow is no longer recommended, but using standard flow leads to a 401 at the keycloak server
+      // when retrieving the token with the access code - we leave it like this for the moment until a solution is found
+      flow: 'implicit'
+    },
+    providers: [
+      {
+        provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+        useFactory: async (configService: ConfigService)=> {
+          await configService.loadConfig();
+          createAuthTokenCondition(configService)
+      }, // Specify conditions for adding the Bearer token
+        deps: [ConfigService]
+      }
+    ]
+  });
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    keycloakProvider,
     provideExperimentalZonelessChangeDetection(),
     provideHttpClient(withInterceptors([includeBearerTokenInterceptor])),
     provideRouter(routes),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    // also inits app config
+    provideConfiguredKeycloak()
   ]
 };
